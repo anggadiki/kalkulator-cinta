6c6bc42e73d1b9f967cf7ec458cc61e20dae9480
anggadiki <anggadikisaputraagung@gmail.com>
2024-06-29 17:30:54 +0700

    set up and config

---
+++ app/components/HomeScreen.tsx
@@ -0,0 +1,13 @@
+const HomeScreen = () => {
+  return (
+    <>
+      <div className=" flex justify-center items-center min-h-screen">
+        <div className="">
+          <h1>Home</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeScreen;
